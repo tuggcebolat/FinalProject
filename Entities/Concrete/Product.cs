762ed0daf9e4922dbb997666cfb47f00f2e71318
tuggcebolat <tuggce20@gmail.com>
2023-07-19 17:14:39 +0300

    updated

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    internal class Product
+    public class Product :IEntity
     {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock{ get; set; }
+        public decimal UnitPrice{ get; set; }
     }
 }
